90e9f484ac6841658cb743e472a73a242f648130
mtotodev05 <mtotodev05@gmail.com>
2017-06-15 20:43:23 +0300

    Toast

--- app/src/main/java/com/example/brian_kamau/calculator/MainActivity.java
+++ app/src/main/java/com/example/brian_kamau/calculator/MainActivity.java
@@ -6,6 +6,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.MissingResourceException;
 
 public class MainActivity extends AppCompatActivity {
     TextView result;
@@ -29,42 +33,91 @@ public class MainActivity extends AppCompatActivity {
 
         add.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)  {
+
+                if(num1.getText().equals(null)){
+                    Toast.makeText(MainActivity.this,"Enter Number",Toast.LENGTH_LONG).show();
+
+                    return;
+                }
+                if(num2.getText().equals(null)){
+                    num1.toString();
+                    num1.setText("Enter Number");
+                    return;
+                }
                 no1=Integer.parseInt(num1.getText().toString());
                 no2=Integer.parseInt(num2.getText().toString());
                 result_num=no1+no2;
 
                 result.setText(String.valueOf(result_num));
+                Toast.makeText(MainActivity.this,"Success",Toast.LENGTH_LONG).show();
+
             }
         });
         substract.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                if(num1.getText()==null){
+                    num1.setText("Enter Number");
+
+                    return;
+                }
+                if(num2.getText()==null){
+                    num1.setText("Enter Number");
+
+                    return;
+                }
                 no1=Integer.parseInt(num1.getText().toString());
                 no2=Integer.parseInt(num2.getText().toString());
                 result_num=no1-no2;
 
                 result.setText(String.valueOf(result_num));
+                Toast.makeText(MainActivity.this,"Success",Toast.LENGTH_LONG).show();
+
             }
         });
         div.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                if(num1.getText()==null){
+                    num1.setText("Enter Number");
+
+                    return;
+                }
+                if(num2.getText()==null){
+                    num1.setText("Enter Number");
+
+                    return;
+                }
                 no1=Integer.parseInt(num1.getText().toString());
                 no2=Integer.parseInt(num2.getText().toString());
                 result_num=no1/no2;
 
                 result.setText(String.valueOf(result_num));
+                Toast.makeText(MainActivity.this,"Success",Toast.LENGTH_LONG).show();
+
             }
         });
         multiply.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)  throws MissingResourceException{
+                if(num1.getText()==null){
+                    num1.setText("Enter Number");
+
+                    return;
+                }
+                if(num2.getText()==null){
+                    num1.setText("Enter Number");
+
+                    return;
+                }
                 no1=Integer.parseInt(num1.getText().toString());
                 no2=Integer.parseInt(num2.getText().toString());
                 result_num=no1*no2;
 
                 result.setText(String.valueOf(result_num));
+                Toast.makeText(MainActivity.this,"Success",Toast.LENGTH_LONG).show();
+
             }
         });
 
